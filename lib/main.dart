b3d9e84b9d9d97465f2f196ba852752a3fb06071
Abhiroop25902 <abhiroop.m25902@gmail.com>
2023-06-29 18:28:41 +0530

    add: firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shopping_list/firebase_options.dart';
 import 'package:shopping_list/widgets/grocery_list.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
@@ -11,7 +21,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    final List<AuthProvider<AuthListener, AuthCredential>> providers = [
+      EmailAuthProvider(),
+      GoogleProvider(clientId: dotenv.get('GOOGLE_CLIENT_ID'))
+    ];
+
     return MaterialApp(
+      initialRoute: FirebaseAuth.instance.currentUser == null
+          ? '/sign-in'
+          : '/grocery-list',
+      routes: {
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: providers,
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/grocery-list');
+              }),
+            ],
+          );
+        },
+        '/grocery-list': (context) {
+          return const GroceryList();
+        },
+      },
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
         useMaterial3: true,
@@ -22,7 +56,6 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: const GroceryList(),
     );
   }
 }
